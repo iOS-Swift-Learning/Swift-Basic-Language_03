e6956049991373c4f58f7be891b354c41aeb09ee
XCODE <XCODE@FM-D2deiMac.local>
2015-09-24 18:46:11 +0800

    Initial Commit

---
+++ SecondClass/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SecondClass
+//
+//  Created by XCODE on 2015/9/24.
+//  Copyright (c) 2015年 Nick. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
